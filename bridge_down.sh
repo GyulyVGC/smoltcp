490391d8a68ab4feef70ad7c47324ba885dd5597
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2023-12-01 17:02:25 +0100

    created scripts to setup and teardown bridged connection

---
+++ bridge_down.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+killall dhcpcd
+ip link set br0 down
+brctl delbr br0
---
+++ bridge_up.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ETH=ens18
+
+modprobe bridge
+modprobe br_netfilter
+
+sysctl -w net.bridge.bridge-nf-call-arptables=0
+sysctl -w net.bridge.bridge-nf-call-ip6tables=0
+sysctl -w net.bridge.bridge-nf-call-iptables=0
+
+ip tuntap add name tap0 mode tap user $USER
+brctl addbr br0
+brctl addif br0 tap0
+brctl addif br0 $ETH
+ip link set tap0 up
+ip link set $ETH up
+ip link set br0 up
+
+sudo dhcpcd br0
