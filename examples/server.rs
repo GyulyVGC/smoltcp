c8cf84b147a8d10dfc2937c259234cc019fd1d80
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2023-12-05 10:21:00 +0100

    fixing server example

--- examples/server.rs
+++ examples/server.rs
@@ -37,7 +37,7 @@ fn main() {
     let mut iface = Interface::new(config, &mut device, Instant::now());
     iface.update_ip_addrs(|ip_addrs| {
         ip_addrs
-            .push(IpCidr::new(IpAddress::v4(192, 168, 69, 1), 24))
+            .push(IpCidr::new(IpAddress::v4(192, 168, 1, 166), 24))
             .unwrap();
         ip_addrs
             .push(IpCidr::new(IpAddress::v6(0xfdaa, 0, 0, 0, 0, 0, 0, 1), 64))
